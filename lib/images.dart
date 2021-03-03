c7663ad2eade0a2c777f431a6a9ce51c3ff68df0
EdwardObala1 <55123555+EdwardObala1@users.noreply.github.com>
2021-03-03 23:01:29 +0300

    Add files via upload

---
+++ lib/images.dart
@@ -0,0 +1,51 @@
+const TEAM_DATA = [
+  {
+    "name": "Home Shirt 20/21",
+    "brand": "Kit",
+    "price": 25.00,
+    "image": "home.jpg"
+  },
+  {
+    "name": "Away Shirt 20/21",
+    "brand": "Kit",
+    "price": 25.00,
+    "image": "away.jpg"
+  },
+  {
+    "name": "Winners Ball",
+    "brand": "Nike",
+    "price": 30.00,
+    "image": "ball.jpg"
+  },
+  {"name": "Shoes", "brand": "Trackers", "price": 20.00, "image": "shoes.jpg"},
+  {
+    "name": "Team Flag",
+    "brand": "Fan Merch",
+    "price": 5.49,
+    "image": "flag.jpg"
+  },
+  {
+    "name": "Puffer Jacket",
+    "brand": "overcoat",
+    "price": 30.49,
+    "image": "puffer.jpg"
+  },
+  {
+    "name": "The Chosen One ",
+    "brand": "Mourinho Poster",
+    "price": 9.99,
+    // "image":"choseone.jpg"
+  },
+  {
+    "name": "Fabregas poster",
+    "brand": "Fabregas poster",
+    "price": 2.99,
+    "image": "sandwich.png"
+  },
+  {
+    "name": "Training Jacket",
+    "brand": "Nike Team Jacket",
+    "price": 19.99,
+    "image": "jacket.jpg"
+  }
+];
